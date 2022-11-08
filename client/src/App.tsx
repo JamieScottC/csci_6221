691f746141953b8f6c11fd2113590da68a9dd710
Jamie Scott <scott.jams@gmail.com>
2022-11-08 15:13:04 -0500

    setup firebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCuPD-XQDbOarnndkzu3VaMtSWBXqdrJQM",
+  authDomain: "csci6221-e2df0.firebaseapp.com",
+  projectId: "csci6221-e2df0",
+  storageBucket: "csci6221-e2df0.appspot.com",
+  messagingSenderId: "679358757872",
+  appId: "1:679358757872:web:7203c64f2ca5023c0c7b35",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 function App() {
   return (
     <div className="App">
